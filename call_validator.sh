0b4e6cc0f9ff8d21d2beeb4e36d41b5dfc5e6a86
yogi <yogiqhz@gmail.com>
2023-11-06 12:02:26 -0300

    Create call_validator.sh

---
+++ call_validator.sh
@@ -0,0 +1,60 @@
+./lights_out < Mapas/Ex-Map.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/0.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/1.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/2.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/3.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/4.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/5.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/6.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/7.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/8.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/9.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/10.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/11.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/12.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/13.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/14.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/15.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/16.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/17.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/18.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/19.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/20.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/21.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/22.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/23.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/24.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/25.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/31.txt > plan.out; \
+python validator.py; \
+./lights_out < Mapas/37.txt > plan.out; \
+python validator.py ; \
+./lights_out < Mapas/Opt-Test.txt > plan.out; \
+python validator.py
