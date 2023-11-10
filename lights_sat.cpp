e790b129c76deb958778bcbb60c28db05bccbf13
yogi <yogiqhz@gmail.com>
2023-11-10 11:06:29 -0300

    Update lights_sat.cpp

--- lights_sat.cpp
+++ lights_sat.cpp
@@ -4,369 +4,32 @@
 
 std::string str_0 = "(define (problem lights_out_problem) (:domain lights_out)\n(:objects\n";
 std::string str_1 = ")\n\n(:init\n";
-std::string str_2 = ")\n\n(:goal (and\n";
+std::string str_2 = ")\n\n(:goal (forall (?pos) (not (is_on ?pos))))\n)\n";
 
 std::string domain_text = "(define (domain lights_out) \n\
-(:requirements :strips :negative-preconditions :equality)\n\
+(:requirements :strips :negative-preconditions :conditional-effects :universal-preconditions) \n\
 \n\
-(:predicates\n\
-    (is_adjascent ?a ?b) ; 'b' está ao lado de 'a'\n\
-    (is_on ?a) ; 'a' está ligado\n\
-    (is_broken ?a) ; 'a' está quebrado (ao ser clicado não vai inverter seu estado)\n\
-    (was_clicked ?a)\n\
-)\n\
+(:predicates \n\
+    (is_adjascent ?a ?b) ; 'b' está ao lado de 'a' \n\
+    (is_on ?a) ; 'a' está ligado \n\
+    (is_broken ?a) ; 'a' está quebrado (ao ser clicado não vai inverter seu estado) \n\
+    ;(was_clicked ?a) \n\
+) \n\
 \n\
-(:action click-off-1 \n\
-    :parameters (?a ?b ?c ?d ?e) \n\
-    :precondition (and\n\
-        (not (was_clicked ?a))\n\
-        (not (= ?a ?b)) (not (= ?a ?c)) (not (= ?a ?d)) (not (= ?a ?e))\n\
-        (not (= ?b ?c)) (not (= ?b ?d)) (not (= ?b ?e))\n\
-        (not (= ?c ?d)) (not (= ?c ?e))\n\
-        (not (= ?d ?e))\n\
-        (is_adjascent ?a ?b)\n\
-        (is_adjascent ?a ?c)\n\
-        (is_adjascent ?a ?d)\n\
-        (is_adjascent ?a ?e)\n\
-        (not (is_broken ?a))\n\
-        (not (is_on ?a))\n\
-        (is_on ?b)\n\
-        (not (is_on ?c))\n\
-        (not (is_on ?d))\n\
-        (not (is_on ?e))\n\
-    )\n\
+(:action click ; ao clicar num quadrado, inverte o estado dos em volta dele e se ele não for quebrado também inverte seu estado \n\
+    :parameters (?a) ; quadrado clicado \n\
+    ;:precondition (not (was_clicked ?a)) \n\
 \n\
-    :effect (and\n\
-        (was_clicked ?a)\n\
-        (is_on ?a)\n\
-        (not (is_on ?b))\n\
-        (is_on ?c)\n\
-        (is_on ?d)\n\
-        (is_on ?e)\n\
-    )\n\
-)\n\
-\n\
-(:action click-off-2\n\
-    :parameters (?a ?b ?c ?d ?e) \n\
-    :precondition (and\n\
-        (not (was_clicked ?a))\n\
-        (not (= ?a ?b)) (not (= ?a ?c)) (not (= ?a ?d)) (not (= ?a ?e))\n\
-        (not (= ?b ?c)) (not (= ?b ?d)) (not (= ?b ?e))\n\
-        (not (= ?c ?d)) (not (= ?c ?e))\n\
-        (not (= ?d ?e))\n\
-        (is_adjascent ?a ?b)\n\
-        (is_adjascent ?a ?c)\n\
-        (is_adjascent ?a ?d)\n\
-        (is_adjascent ?a ?e)\n\
-        (not (is_broken ?a))\n\
-        (not (is_on ?a))\n\
-        (is_on ?b)\n\
-        (is_on ?c)\n\
-        (not (is_on ?d))\n\
-        (not (is_on ?e))\n\
-    )\n\
-\n\
-    :effect (and\n\
-        (was_clicked ?a)\n\
-        (is_on ?a)\n\
-        (not (is_on ?b))\n\
-        (not (is_on ?c))\n\
-        (is_on ?d)\n\
-        (is_on ?e)\n\
-    )\n\
-)\n\
-\n\
-(:action click-off-3\n\
-    :parameters (?a ?b ?c ?d ?e) \n\
-    :precondition (and\n\
-        (not (was_clicked ?a))\n\
-        (not (= ?a ?b)) (not (= ?a ?c)) (not (= ?a ?d)) (not (= ?a ?e))\n\
-        (not (= ?b ?c)) (not (= ?b ?d)) (not (= ?b ?e))\n\
-        (not (= ?c ?d)) (not (= ?c ?e))\n\
-        (not (= ?d ?e))\n\
-        (is_adjascent ?a ?b)\n\
-        (is_adjascent ?a ?c)\n\
-        (is_adjascent ?a ?d)\n\
-        (is_adjascent ?a ?e)\n\
-        (not (is_broken ?a))\n\
-        (not (is_on ?a))\n\
-        (is_on ?b)\n\
-        (is_on ?c)\n\
-        (is_on ?d)\n\
-        (not (is_on ?e))\n\
-    )\n\
-\n\
-    :effect (and\n\
-        (was_clicked ?a)\n\
-        (is_on ?a)\n\
-        (not (is_on ?b))\n\
-        (not (is_on ?c))\n\
-        (not (is_on ?d))\n\
-        (is_on ?e)\n\
-    )\n\
-)\n\
-\n\
-(:action click-off-4\n\
-    :parameters (?a ?b ?c ?d ?e) \n\
-    :precondition (and\n\
-        (not (was_clicked ?a))\n\
-        (not (= ?a ?b)) (not (= ?a ?c)) (not (= ?a ?d)) (not (= ?a ?e))\n\
-        (not (= ?b ?c)) (not (= ?b ?d)) (not (= ?b ?e))\n\
-        (not (= ?c ?d)) (not (= ?c ?e))\n\
-        (not (= ?d ?e))\n\
-        (is_adjascent ?a ?b)\n\
-        (is_adjascent ?a ?c)\n\
-        (is_adjascent ?a ?d)\n\
-        (is_adjascent ?a ?e)\n\
-        (not (is_broken ?a))\n\
-        (not (is_on ?a))\n\
-        (is_on ?b)\n\
-        (is_on ?c)\n\
-        (is_on ?d)\n\
-        (is_on ?e)\n\
-    )\n\
-\n\
-    :effect (and\n\
-        (was_clicked ?a)\n\
-        (is_on ?a)\n\
-        (not (is_on ?b))\n\
-        (not (is_on ?c))\n\
-        (not (is_on ?d))\n\
-        (not (is_on ?e))\n\
-    )\n\
-)\n\
-\n\
-(:action click-on-1 \n\
-    :parameters (?a ?b ?c ?d ?e) \n\
-    :precondition (and\n\
-        (not (was_clicked ?a))\n\
-        (not (= ?a ?b)) (not (= ?a ?c)) (not (= ?a ?d)) (not (= ?a ?e))\n\
-        (not (= ?b ?c)) (not (= ?b ?d)) (not (= ?b ?e))\n\
-        (not (= ?c ?d)) (not (= ?c ?e))\n\
-        (not (= ?d ?e))\n\
-        (is_adjascent ?a ?b)\n\
-        (is_adjascent ?a ?c)\n\
-        (is_adjascent ?a ?d)\n\
-        (is_adjascent ?a ?e)\n\
-        (not (is_broken ?a))\n\
-        (is_on ?a)\n\
-        (is_on ?b)\n\
-        (not (is_on ?c))\n\
-        (not (is_on ?d))\n\
-        (not (is_on ?e))\n\
-    )\n\
-\n\
-    :effect (and\n\
-        (was_clicked ?a)\n\
-        (not (is_on ?a))\n\
-        (not (is_on ?b))\n\
-        (is_on ?c)\n\
-        (is_on ?d)\n\
-        (is_on ?e)\n\
-    )\n\
-)\n\
-\n\
-(:action click-on-2\n\
-    :parameters (?a ?b ?c ?d ?e) \n\
-    :precondition (and\n\
-        (not (was_clicked ?a))\n\
-        (not (= ?a ?b)) (not (= ?a ?c)) (not (= ?a ?d)) (not (= ?a ?e))\n\
-        (not (= ?b ?c)) (not (= ?b ?d)) (not (= ?b ?e))\n\
-        (not (= ?c ?d)) (not (= ?c ?e))\n\
-        (not (= ?d ?e))\n\
-        (is_adjascent ?a ?b)\n\
-        (is_adjascent ?a ?c)\n\
-        (is_adjascent ?a ?d)\n\
-        (is_adjascent ?a ?e)\n\
-        (not (is_broken ?a))\n\
-        (is_on ?a)\n\
-        (is_on ?b)\n\
-        (is_on ?c)\n\
-        (not (is_on ?d))\n\
-        (not (is_on ?e))\n\
-    )\n\
-\n\
-    :effect (and\n\
-        (was_clicked ?a)\n\
-        (not (is_on ?a))\n\
-        (not (is_on ?b))\n\
-        (not (is_on ?c))\n\
-        (is_on ?d)\n\
-        (is_on ?e)\n\
-    )\n\
-)\n\
-\n\
-(:action click-on-3\n\
-    :parameters (?a ?b ?c ?d ?e) \n\
-    :precondition (and\n\
-        (not (was_clicked ?a))\n\
-        (not (= ?a ?b)) (not (= ?a ?c)) (not (= ?a ?d)) (not (= ?a ?e))\n\
-        (not (= ?b ?c)) (not (= ?b ?d)) (not (= ?b ?e))\n\
-        (not (= ?c ?d)) (not (= ?c ?e))\n\
-        (not (= ?d ?e))\n\
-        (is_adjascent ?a ?b)\n\
-        (is_adjascent ?a ?c)\n\
-        (is_adjascent ?a ?d)\n\
-        (is_adjascent ?a ?e)\n\
-        (not (is_broken ?a))\n\
-        (is_on ?a)\n\
-        (is_on ?b)\n\
-        (is_on ?c)\n\
-        (is_on ?d)\n\
-        (not (is_on ?e))\n\
-    )\n\
-\n\
-    :effect (and\n\
-        (was_clicked ?a)\n\
-        (not (is_on ?a))\n\
-        (not (is_on ?b))\n\
-        (not (is_on ?c))\n\
-        (not (is_on ?d))\n\
-        (is_on ?e)\n\
-    )\n\
-)\n\
-\n\
-(:action click-on-4\n\
-    :parameters (?a ?b ?c ?d ?e) \n\
-    :precondition (and\n\
-        (not (was_clicked ?a))\n\
-        (not (= ?a ?b)) (not (= ?a ?c)) (not (= ?a ?d)) (not (= ?a ?e))\n\
-        (not (= ?b ?c)) (not (= ?b ?d)) (not (= ?b ?e))\n\
-        (not (= ?c ?d)) (not (= ?c ?e))\n\
-        (not (= ?d ?e))\n\
-        (is_adjascent ?a ?b)\n\
-        (is_adjascent ?a ?c)\n\
-        (is_adjascent ?a ?d)\n\
-        (is_adjascent ?a ?e)\n\
-        (not (is_broken ?a))\n\
-        (is_on ?a)\n\
-        (is_on ?b)\n\
-        (is_on ?c)\n\
-        (is_on ?d)\n\
-        (is_on ?e)\n\
-    )\n\
-\n\
-    :effect (and\n\
-        (was_clicked ?a)\n\
-        (not (is_on ?a))\n\
-        (not (is_on ?b))\n\
-        (not (is_on ?c))\n\
-        (not (is_on ?d))\n\
-        (not (is_on ?e))\n\
-    )\n\
-)\n\
-\n\
-(:action click-broken-1 \n\
-    :parameters (?a ?b ?c ?d ?e) \n\
-    :precondition (and\n\
-        (not (was_clicked ?a))\n\
-        (not (= ?a ?b)) (not (= ?a ?c)) (not (= ?a ?d)) (not (= ?a ?e))\n\
-        (not (= ?b ?c)) (not (= ?b ?d)) (not (= ?b ?e))\n\
-        (not (= ?c ?d)) (not (= ?c ?e))\n\
-        (not (= ?d ?e))\n\
-        (is_adjascent ?a ?b)\n\
-        (is_adjascent ?a ?c)\n\
-        (is_adjascent ?a ?d)\n\
-        (is_adjascent ?a ?e)\n\
-        (is_broken ?a)\n\
-        (is_on ?b)\n\
-        (not (is_on ?c))\n\
-        (not (is_on ?d))\n\
-        (not (is_on ?e))\n\
-    )\n\
-\n\
-    :effect (and\n\
-        (was_clicked ?a)\n\
-        (not (is_on ?b))\n\
-        (is_on ?c)\n\
-        (is_on ?d)\n\
-        (is_on ?e)\n\
-    )\n\
-)\n\
-\n\
-(:action click-broken-2\n\
-    :parameters (?a ?b ?c ?d ?e) \n\
-    :precondition (and\n\
-        (not (was_clicked ?a))\n\
-        (not (= ?a ?b)) (not (= ?a ?c)) (not (= ?a ?d)) (not (= ?a ?e))\n\
-        (not (= ?b ?c)) (not (= ?b ?d)) (not (= ?b ?e))\n\
-        (not (= ?c ?d)) (not (= ?c ?e))\n\
-        (not (= ?d ?e))\n\
-        (is_adjascent ?a ?b)\n\
-        (is_adjascent ?a ?c)\n\
-        (is_adjascent ?a ?d)\n\
-        (is_adjascent ?a ?e)\n\
-        (is_broken ?a)\n\
-        (is_on ?b)\n\
-        (is_on ?c)\n\
-        (not (is_on ?d))\n\
-        (not (is_on ?e))\n\
-    )\n\
-\n\
-    :effect (and\n\
-        (was_clicked ?a)\n\
-        (not (is_on ?b))\n\
-        (not (is_on ?c))\n\
-        (is_on ?d)\n\
-        (is_on ?e)\n\
-    )\n\
-)\n\
-\n\
-(:action click-broken-3\n\
-    :parameters (?a ?b ?c ?d ?e) \n\
-    :precondition (and\n\
-        (not (was_clicked ?a))\n\
-        (not (= ?a ?b)) (not (= ?a ?c)) (not (= ?a ?d)) (not (= ?a ?e))\n\
-        (not (= ?b ?c)) (not (= ?b ?d)) (not (= ?b ?e))\n\
-        (not (= ?c ?d)) (not (= ?c ?e))\n\
-        (not (= ?d ?e))\n\
-        (is_adjascent ?a ?b)\n\
-        (is_adjascent ?a ?c)\n\
-        (is_adjascent ?a ?d)\n\
-        (is_adjascent ?a ?e)\n\
-        (is_broken ?a)\n\
-        (is_on ?b)\n\
-        (is_on ?c)\n\
-        (is_on ?d)\n\
-        (not (is_on ?e))\n\
-    )\n\
-\n\
-    :effect (and\n\
-        (was_clicked ?a)\n\
-        (not (is_on ?b))\n\
-        (not (is_on ?c))\n\
-        (not (is_on ?d))\n\
-        (is_on ?e)\n\
-    )\n\
-)\n\
-\n\
-(:action click-broken-4\n\
-    :parameters (?a ?b ?c ?d ?e) \n\
-    :precondition (and\n\
-        (not (was_clicked ?a))\n\
-        (not (= ?a ?b)) (not (= ?a ?c)) (not (= ?a ?d)) (not (= ?a ?e))\n\
-        (not (= ?b ?c)) (not (= ?b ?d)) (not (= ?b ?e))\n\
-        (not (= ?c ?d)) (not (= ?c ?e))\n\
-        (not (= ?d ?e))\n\
-        (is_adjascent ?a ?b)\n\
-        (is_adjascent ?a ?c)\n\
-        (is_adjascent ?a ?d)\n\
-        (is_adjascent ?a ?e)\n\
-        (is_broken ?a)\n\
-        (is_on ?b)\n\
-        (is_on ?c)\n\
-        (is_on ?d)\n\
-        (is_on ?e)\n\
-    )\n\
-\n\
-    :effect (and\n\
-        (was_clicked ?a)\n\
-        (not (is_on ?b))\n\
-        (not (is_on ?c))\n\
-        (not (is_on ?d))\n\
-        (not (is_on ?e))\n\
-    )\n\
-)\n\
+    :effect (and ; inverte o estado do quadrado clicado se ele não estiver quebrado \n\
+        ;(was_clicked ?a) \n\
+        (when (and (not (is_broken ?a)) (not (is_on ?a))) (is_on ?a)) \n\
+        (when (and (not (is_broken ?a)) (is_on ?a)) (not (is_on ?a))) \n\
+        (forall (?b) (and ; inverte o estado dos quadrados ao lado do clicado \n\
+            (when (and (is_adjascent ?a ?b) (not (is_on ?b))) (is_on ?b)) \n\
+            (when (and (is_adjascent ?a ?b) (is_on ?b)) (not (is_on ?b))) \n\
+        )) \n\
+    ) \n\
+) \n\
 )\n";
 
 int main() {
@@ -386,9 +49,9 @@ int main() {
     int map_dimension = sqrt(map.size()); // Mapas são n x n.
 
     // Escrever cada posição do mapa.
-    for (int i = -1; i <= map_dimension; ++i) {
-        for (int j = -1; j <= map_dimension; ++j) {
-            if (j == -1)
+    for (int i = 0; i < map_dimension; ++i) {
+        for (int j = 0; j < map_dimension; ++j) {
+            if (j == 0)
                 std::fwrite("\t", 1, 1, problem);
 
             std::string pos = "p" + std::to_string(i) + "-" + std::to_string(j) + " ";
@@ -405,9 +68,9 @@ int main() {
     for (int i = 0; i < map_dimension; ++i) {
         for (int j = 0; j < map_dimension; ++j) {
             std::string pos = "p" + std::to_string(i) + "-" + std::to_string(j);
-	    char bulb = map[i * map_dimension + j];
+            char bulb = map[i * map_dimension + j];
             
-	    if (bulb == 'L' or bulb == 'l') {
+	        if (bulb == 'L' or bulb == 'l') {
                 std::string predicate = "\t(is_on " + pos + ")\n";
                 std::fwrite(predicate.c_str(), predicate.size(), 1, problem);
             }
@@ -422,14 +85,14 @@ int main() {
     std::fwrite("\n", 1, 1, problem);
 
     // Escrever as posições adjascentes.
-    for (int i = -1; i <= map_dimension; ++i) {
-        for (int j = -1, a = -2; j <= map_dimension; ++j) {
+    for (int i = 0; i < map_dimension; ++i) {
+        for (int j = 0, a = -1; j < map_dimension; ++j) {
             int dirs[] = { -1, 0, 1, 0, -1 };
 
             for (int k = 0; k < 4; ++k) {
                 int x = j + dirs[k], y = i + dirs[k + 1];
 
-                if (x < -1 or x > map_dimension + 1 or y < -1 or y > map_dimension + 1)
+                if (x < 0 or x > map_dimension or y < 0 or y > map_dimension)
                     continue;
 
 		std::string predicate;
@@ -455,19 +118,6 @@ int main() {
 
     // Escrever goal.
     std::fwrite(str_2.c_str(), str_2.size(), 1, problem);
-    for (int i = 0; i < map_dimension; ++i) {
-        for (int j = 0; j < map_dimension; ++j) {
-            if (j == 0)
-                std::fwrite("\t", 1, 1, problem);
-
-            std::string pos = "(not (is_on p" + std::to_string(i) + "-" + std::to_string(j) + ")) ";
-            std::fwrite(pos.c_str(), pos.size(), 1, problem);
-        }
-
-        std::fwrite("\n", 1, 1, problem);
-    }
-    std::string str_3 = "\t)\n)\n)";
-    std::fwrite(str_3.c_str(), str_3.size(), 1, problem);
     std::fclose(problem);
 
     // Criar e escrever domínio:
@@ -476,20 +126,24 @@ int main() {
     std::fclose(domain);
 
     // Chamar planejador:
-    system("/tmp/dir/software/planners/madagascar/M -S 1 -Q domain.pddl problem.pddl -o plan.pddl > /dev/null 2>&1");
+    system("/tmp/dir/software/planners/downward-fdss23/fast-downward.py --alias seq-sat-fdss-2023 --overall-time-limit 895 --plan-file plan.pddl domain.pddl problem.pddl > /dev/null 2>&1");
     remove("domain.pddl");
     remove("problem.pddl");
 
-    FILE* plan = std::fopen("plan.pddl", "r");
+    int i = 2;
+    FILE* plan_buffer = std::fopen("plan.pddl.1", "r"), *plan = plan_buffer;
+    while (plan_buffer != NULL) {
+        std::string plan_name = "plan.pddl." + std::to_string(i);
+        plan_buffer = std::fopen(plan_name.c_str(), "r");
+        if (plan_buffer == NULL)
+            break;
+        plan = plan_buffer;
+        ++i;
+        }
 
-    bool primeiro = true, in_click = false, readingX = false, readingY = false;
+    bool primeiro = true, readingX = false, readingY = false;
     while ((c  = std::fgetc(plan)) != EOF) {
-       if (c == 'c') {
-	   in_click = true;
-	   continue;
-       }
-
-       if (in_click and c == 'p') {
+       if (c == 'p') {
            readingY = true;
 
            if (primeiro) {
@@ -512,9 +166,8 @@ int main() {
            std::cout << c;
        }
        else if (readingX) {
-           if (c == ' ') {
+           if (c == ')') {
 	       readingX = false;
-	       in_click = false;
 	       std::cout << ")";
                continue;
 	   }
@@ -525,5 +178,5 @@ int main() {
 
     std::cout << std::endl;
     std::fclose(plan);
-    remove("plan.pddl");
+    system("/tmp/dir/software/planners/downward-fdss23/fast-downward.py --plan-file plan.pddl --cleanup");
 }
